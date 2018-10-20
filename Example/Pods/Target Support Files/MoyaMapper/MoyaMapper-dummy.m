b12778c7898b34e298c173c13cbec28e12ddcca0
LinXunFeng <598600855@qq.com>
2018-10-20 08:53:10 +0800

    update .gitignore

--- Example/Pods/Target Support Files/MoyaMapper/MoyaMapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MoyaMapper : NSObject
-@end
-@implementation PodsDummy_MoyaMapper
-@end
--- Example/Pods/Target Support Files/Pods-MoyaMapper_Tests/Pods-MoyaMapper_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoyaMapper_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_MoyaMapper_Tests
-@end
